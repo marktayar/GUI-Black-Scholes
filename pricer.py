872ac973fa6739017d11be5bbd0db8b32ca2e6f2
marktayar <mark.tayar@epfl.ch>
2024-12-06 19:04:22 +0100

    maps

--- pricer.py
+++ pricer.py
@@ -3,7 +3,8 @@ import streamlit as st
 import pandas as pd
 from scipy.stats import norm
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
