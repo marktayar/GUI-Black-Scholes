cdb306bdb3cf550bace807bdedd0625f777347cf
marktayar <mark.tayar@epfl.ch>
2024-12-06 19:21:24 +0100

    trial

--- pricer.py
+++ pricer.py
@@ -5,12 +5,13 @@ from scipy.stats import norm
 
 # Add a selectbox to the sidebar:
 add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
+    'What kind of option would you like?',
+    ('Call', 'Put')
 )
 
 # Add a slider to the sidebar:
 add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
+    'Current Price',
+    0.01, 1000, 25
 )
+
