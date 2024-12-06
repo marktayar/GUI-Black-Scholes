26d50b89e5b2f0ced00dd6722527df4d4a51afed
marktayar <mark.tayar@epfl.ch>
2024-12-06 19:09:24 +0100

    name

--- pricer.py
+++ pricer.py
@@ -3,5 +3,7 @@ import streamlit as st
 import pandas as pd
 from scipy.stats import norm
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
