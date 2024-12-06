4a3babf09425729dccc8254ca31cf81af683da5b
marktayar <mark.tayar@epfl.ch>
2024-12-06 19:35:17 +0100

    trial5

--- pricer.py
+++ pricer.py
@@ -8,11 +8,18 @@ add_selectbox = st.sidebar.selectbox(
     'What kind of option would you like?',
     ('Call', 'Put')
 )
+'You selected: ', add_selectbox
 
 add_textbox_price = st.sidebar.text_input("Current Price" , key = "current_price")
+'You selected: ', st.session_state.current_price
 add_textbox_strike = st.sidebar.text_input("Strike Price" , key = "strike_price")
-add_textbox_matu = st.sidebar.text_input("Time to Maturity" , key = "Maturity")
+'You selected: ', st.session_state.strike_price
+add_textbox_matu = st.sidebar.text_input("Time to Maturity" , key = "maturity")
+'You selected: ', st.session_state.maturity
 add_textbox_sigma = st.sidebar.text_input("Volatility in percentage" , key = "vol")
+'You selected: ', st.session_state.vol ,'%'
 add_textbox_rf = st.sidebar.text_input("Risk free rate in percentage" , key = "rf")
+'You selected: ', st.session_state.rf ,'%'
 add_textbox_dividend = st.sidebar.text_input("Dividend Rate in percentage" , key = "dividend_rate")
+'You selected: ', st.session_state.dividend_rate ,'%'
 
